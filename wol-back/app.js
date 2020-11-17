fa92356643e8a1d319719d34de90389f0c1258ab
gimdh <36066149+gimdh@users.noreply.github.com>
2020-11-17 16:05:39 +0900

    Fix missing favicon

--- wol-back/app.js
+++ wol-back/app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
